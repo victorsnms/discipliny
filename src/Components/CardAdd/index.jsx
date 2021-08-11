dd5a87062e23715e42050c11957d51fdb85f16c7
Rafael Leonardo <rafael.zoe26@gmail.com>
2021-08-11 18:35:12 -0300

    feature: adicionado páginass: Groups e MyGroups

---
+++ src/Components/CardAdd/index.jsx
@@ -0,0 +1,13 @@
+import { CardAddWrapper } from "./styles";
+
+const CardAdd = () => {
+  return (
+    <CardAddWrapper className="ContentHabits">
+      <div className="add">
+        <p>+</p>
+      </div>
+    </CardAddWrapper>
+  );
+};
+
+export default CardAdd;
