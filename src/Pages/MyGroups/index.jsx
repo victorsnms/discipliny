5fc6eb3d904141603062d8823fe9e79da58f2c1f
Waldiney <walferreira@gmail.com>
2021-08-17 22:54:25 -0300

    fix: toast on providers

--- src/Pages/MyGroups/index.jsx
+++ src/Pages/MyGroups/index.jsx
@@ -1,7 +1,6 @@
 import { Redirect } from "react-router-dom";
 import { MyGroupsWrapper } from "./myGroups";
 import CardGroups from "../../Components/CardGroups";
-import CardAdd from "../../Components/CardAdd";
 import MenuMobile from "../../Components/MenuMobile";
 import Menu from "../../Components/MenuAside/index";
 import { useDisclosure } from "@chakra-ui/react";
@@ -14,8 +13,7 @@ import { useState } from "react";
 function Groups() {
   const { logged } = useLogged();
 
-  const { myGroupsList, setMyGroupsList, createNewGroup, updateMyGroup } =
-    useMyGroups();
+  const { myGroupsList } = useMyGroups();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
--- src/Provider/MyGroups/index.js
+++ src/Provider/MyGroups/index.js
@@ -6,35 +6,8 @@ const MyGroupsCardListContext = createContext();
 
 export const MyGroupsCardsProvider = ({ children }) => {
   const toast = useToast();
+
   const [myGroupsList, setMyGroupsList] = useState([]);
-
-  const createNewGroup = (dados) => {
-    const token = JSON.parse(localStorage.getItem("@Discipliny:accessToken"));
-    api
-      .post("/groups/", dados, {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((response) => {
-        setMyGroupsList([...myGroupsList, response.data]);
-
-        toast({
-          title: "Grupo criado!",
-          description: "Seu grupo foi criado com sucesso.",
-          status: "success",
-          duration: 2000,
-          isClosable: true,
-        });
-      })
-      .catch((_) => {
-        toast({
-          title: "erro ao Criar grupo!",
-          description: "Falha ao criar novo grupo",
-          status: "error",
-          duration: 2000,
-          isClosable: true,
-        });
-      });
-  };
 
   const getGroups = () => {
     const token = JSON.parse(localStorage.getItem("@Discipliny:accessToken"));
@@ -49,39 +22,41 @@ export const MyGroupsCardsProvider = ({ children }) => {
           JSON.stringify(response.data.id)
         );
       })
-      .catch((_) => {});
+      .catch((error) => {
+        console.log(error);
+      });
   };
   useEffect(() => {
     getGroups();
   }, []);
 
-  const updateMyGroup = (dados, groupId) => {
-    const token = JSON.parse(localStorage.getItem("@Discipliny:accessToken"));
-    api
-      .patch(`/groups/${groupId}`, dados, {
-        headers: { Authorization: `Bearer ${token}` },
-      })
-      .then((_) => {
-        getGroups();
-      })
-      .catch((_) => {
-        toast({
-          title: "erro ao Criar grupo!",
-          description: "Erro ao atualizar grupos",
-          status: "error",
-          duration: 2000,
-          isClosable: true,
-        });
-      });
-  };
+  // const updateMyGroup = (dados, groupId) => {
+  //   const token = JSON.parse(localStorage.getItem("@Discipliny:accessToken"));
+  //   api
+  //     .patch(`/groups/${groupId}`, dados, {
+  //       headers: { Authorization: `Bearer ${token}` },
+  //     })
+  //     .then((_) => {
+  //       getGroups();
+  //     })
+  //     .catch((_) => {
+  //       toast({
+  //         title: "erro ao Criar grupo!",
+  //         description: "Erro ao atualizar grupos",
+  //         status: "error",
+  //         duration: 2000,
+  //         isClosable: true,
+  //       });
+  //     });
+  // };
 
   return (
     <MyGroupsCardListContext.Provider
       value={{
         myGroupsList,
         setMyGroupsList,
-        createNewGroup,
-        updateMyGroup,
+        // createNewGroup,
+        // updateMyGroup,
         getGroups,
       }}
     >
