ca3e04b89be34103a75f92717e67a703e1d9337e
Victor Martins <victorsantows@gmail.com>
2021-08-18 08:26:14 -0300

    fix: card goal and activity sizes

--- src/Components/CardActivity/index.jsx
+++ src/Components/CardActivity/index.jsx
@@ -3,20 +3,21 @@ import ActivitiesDeleteModal from "../ActivitiesDeleteModal";
 import ActivitiesUpdateModal from "../ActivitiesUpdateModal";
 
 const ActivityCard = ({ activity }) => {
-
-  const editDate = new Date(activity.realization_time).toLocaleDateString("pt-BR", {
-    day: "2-digit",
-    month: "short",
-    year: "numeric"
-  })
+  const editDate = new Date(activity.realization_time).toLocaleDateString(
+    "pt-BR",
+    {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+    }
+  );
   return (
     <CardActivity>
       <div className="flex-row">
         <h1 className="activityName">{activity.title}</h1>
 
-        <div>
-          <p>{editDate}</p>
-        </div>
+        <p className="activityDate">{editDate}</p>
+
         <ActivitiesDeleteModal activity={activity} />
         <ActivitiesUpdateModal activity={activity} />
       </div>
